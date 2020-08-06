a115b66316da91c8150a4b920a82450b3ef104bc
JERRYLITTLE <745370327@qq.com>
2020-08-06 23:04:19 +0800

    彭世杰-登录页

---
+++ src/components/Fooder.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-footer>当猫爱上狗</el-footer>
+</template>
+
+<script>
+export default {
+    name:"Footer"
+}
+</script>
+
+<style scoped>
+.el-footer{
+background-color: #FFECCB;
+  color: #333;
+  font-weight: 700;
+  text-align: center;
+  line-height: 60px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+}
+</style>
