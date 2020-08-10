e9021b57a718584ce183bbedec612b8ddfbb87c9
JERRYLITTLE <745370327@qq.com>
2020-08-10 22:18:12 +0800

    psj——支付页面——充值页面设计

---
+++ src/views/pay/payment.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="payment">
+    <el-container style="height=100%;" direction="vertical">
+      <TOPBAR></TOPBAR>
+      <el-main>
+        <div>
+          <el-image style="width: 1100px; height: 500px"  :lazy='true' :src="imgUrl"></el-image>
+        </div>
+        <div style="margin-top:-120px">请使用微信扫码支付</div>
+      </el-main>
+      <FOOTBAR></FOOTBAR>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import TOPBAR from "../layout/Topbar";
+import FOOTBAR from "../layout/Footer";
+export default {
+  name: "payment",
+  components: {
+    TOPBAR,
+    FOOTBAR
+  },
+  data(){
+    return{
+        imgUrl: require("../../assets/images/pay.png")
+    }
+  }
+};
+</script>
+
+<style>
+#payment {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+html,
+body,
+.el-container {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+}
+.el-main {
+  text-align: center;
+  line-height: 160px;
+}
+</style>
---
+++ src/views/pay/rechargePage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="recharge">
+    <el-container style="height=100%;" direction="vertical">
+      <TOPBAR></TOPBAR>
+      <el-main>
+        <div style="margin-top:100px">
+          金额：
+          <el-input placeholder="请输入充值金额" v-model="balance" clearable></el-input>
+        </div>
+        <div style="margin-top:-50px">
+            <el-button type="warning" @click="checkChange">确认充值</el-button>
+        </div>
+      </el-main>
+      <FOOTBAR></FOOTBAR>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import TOPBAR from "../layout/Topbar";
+import FOOTBAR from "../layout/Footer";
+export default {
+  name: "rechargePage",
+  components: {
+    TOPBAR,
+    FOOTBAR,
+  },
+  data() {
+    return {
+      balance: "",
+    };
+  },
+  methods:{
+      //更新数据库中的余额，然后更新个人信息
+      checkChange(){
+         
+      }
+  }
+};
+</script>
+
+<style scoped>
+#recharge {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+html,
+body,
+.el-container {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+}
+.el-main {
+  text-align: center;
+  line-height: 160px;
+}
+.el-input {
+  width: auto;
+}
+</style>
