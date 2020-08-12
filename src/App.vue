e6d2e2447ce8fd1ccaa9f64fd9549a2d1b4ad0d0
JERRYLITTLE <745370327@qq.com>
2020-08-12 14:46:02 +0800

    Signed-off-by: JERRYLITTLE <745370327@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <div id="app">
-    
+<!-- 注意这里为共通样式，不能加scoped -->
+  <div id="app">   
     <router-view/>
   </div>
 </template>
 
-
 <script>
-
-
 export default {
   name:"app",
 }
 </script>
-
 <style>
-
+	/*这里的内容，就是静态工程中的 reset.css*/
+	html,body,div,span,h1,h2,h3,h4,h5,h6,ul,ol,li,p {
+		margin: 0;
+		padding: 0;
+		font-family: "微软雅黑";
+	}
+	html,body,#app {
+		/*必须要设置，这样总容器高度才能100%*/
+		width: 100%;
+		height: 100%;
+	}
+	ul,ol {
+		list-style: none;
+	}
+	a {
+		text-decoration: none;
+	}
 </style>
